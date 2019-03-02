cd860c2b3a6b2c5980a4aa146259afe26c9a5893
Zach <zachary.fey@gmail.com>
2019-03-02 10:16:57 -0500

    moved most of the code to methods

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready();
 
-var goodSelect, badSelect, goodPicked, badPicked, gameOver
+var goodSelect, badSelect, goodPicked, badPicked, gameOver, fightStart
 
 
 
@@ -12,39 +12,50 @@ game = {
         badSelect = null;
         badPicked = false;
         gameOver = false;
+        fightStart = false;
     },
+
+    characterSelect: function(obj){
+        if(goodPicked === false){
+            
+            goodSelect = obj.id;
+            goodPicked = true
+            console.log('good guy: ' + goodSelect);
+
+            $("#goodSelect").hide();
+
+            $(obj).clone().appendTo('#battleground');
+            $(obj).css('float', 'left');
+            $('#badSelect').css('visibility','visible');
+        }  else if(badPicked === false && goodPicked === true){
+            
+            badSelect = obj.id;
+            badPicked = true;
+            console.log('bad guy: '+badSelect);
+
+            $("#badSelect").hide();
+            
+            $(obj).clone().appendTo('#battleground');
+            $(obj).css('float', 'right');
+
+            game.fightStart();
+        }
+    },
+
+    fightStart: function(){
+        $('body').css('background-image','url(assets/images/backgroundSelected.jpg)');
+        $('.arena').css('margin-top', '200px');
+        $("h1").hide();
+        fightStart = true;
+        console.log('fight has started')
+    }
 }
 
 game.initialize();
 console.log('initialize');
 
-    if(goodPicked === false){
-        $('.goodGuy').on('click', function(){
-            goodSelect = this.id;
-            console.log(goodSelect);
-            goodPicked = true
-            $("#goodSelect").hide();
-            console.log(goodPicked);
-            $(this).clone().appendTo('#battleground');
-            $(this).css('float', 'left');
-            $('#badSelect').css('visibility','visible');
-        })
-    }
-    if(badPicked === false){
-        $('.badGuy').on('click', function(){
-            badSelect = this.id;
-            console.log(badSelect);
-            badPicked = true;
-            $("#badSelect").hide();
-            console.log(badPicked);
-            $(this).clone().appendTo('#battleground');
-            $('body').css('background-image','url(assets/images/backgroundSelected.jpg)');
-            $(this).css('float', 'right');
-            $('.arena').css('margin-top', '200px');
-            $("h1").hide();
-        })
-    }
 
-    document.onkeyup = function(event){
-        console.log(goodPicked);
-    }
+$('.goodGuy').on('click', function(){game.characterSelect(this);}) //if a good guy is clicked, check if good guy has been picked yet, and hide fighter select
+
+$('.badGuy').on('click', function(){game.characterSelect(this);}) //if a good guy is clicked, check if good guy and bad guy have been picked yet, and hide enemy select
+
